ebdf1d3a05081a7076d30ca2482825c9d1c76f1b
dinosaurleg <8763690+Dinosaurleg@users.noreply.github.com>
2020-08-18 21:00:19 -0500

    Made sample controller. Switching to sts

---
+++ LearningSpring/src/main/java/com/dinosaurleg/RestControllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.dinosaurleg.RestControllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "This is a test";
+    }
+}
